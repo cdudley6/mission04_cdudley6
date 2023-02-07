3f75e7fc2f96207969cf52d1ef59ffa9a27db3d7
Carson Dudley <cdudley6@byu.edu>
2023-02-06 22:21:03 -0700

    aa

---
+++ MyFirstASP.NETWebApp/controllers/oiiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASP.NETWebApp.controllers
+{
+    public class oiiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+    }
+}
